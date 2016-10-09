f2d94d60aa497e30cc2aa1ffb0fcd9e3eced7d62
Eric Little <reallyimeric@gmail.com>
2016-10-09 15:38:40 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const options = {
+  path: '/chat'
+}
+const io = require('socket.io')(server, options)
+
+const fs = require('fs')
+app.get('/', (req, res) => {
+  const indexHtml = fs.createReadStream('index.html')
+  indexHtml.pipe(res)
+})
+app.use('/public', express.static('public'))
+app.use('/js', express.static('js'))
+
+server.listen(3000, () => {
+  console.log('started listening on *:3000')
+  console.log(`socket path is ${io.path()}`)
+})
+
+io.on('connection', socket => {
+  console.log(io.sockets.connected[socket.id].id)
+  console.log(`${new Date()}: ${socket.id} connected, namespace is ${io.sockets.name}`)
+  socket.on('disconnect', () => {
+    console.log(`${new Date()}: ${socket.id} disconnected`)
+  })
+  socket.on('say', msg => {
+    console.log(`${new Date()}: MSG: ${socket.id} :"${msg}"`)
+    io.emit('broadcast', socket.id, msg)
+  })
+})
